e80c26102d1800cec7671f93cfec619927d2c848
seyed-reza <Sadrzadehsr@gmail.com>
2026-09-06 14:47:07 +0000

    Register password hash migration

--- DataAccess/Migrations/20260906120000_HashKhademPasswords.cs
+++ DataAccess/Migrations/20260906120000_HashKhademPasswords.cs
@@ -1,9 +1,13 @@
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 namespace DataAccess.Migrations
 {
+    [DbContext(typeof(GonbadDbContext))]
+    [Migration("20260906120000_HashKhademPasswords")]
     public partial class HashKhademPasswords : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
